bd52e14d917c3e7430d2d330d0362fe33c1320ab
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-05 01:31:41 -0300

    style: change some values to buttons

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -5,9 +5,9 @@ import './Playlist.scss'
 function Playlist({playlist}) {
   return(
     <div className="PlaylistContent">
-      <input className="Playlist-input" placeholder='playlist name'/>
+      <input className="Playlist-input" placeholder='playlist name' value={playlist.name}/>
       <Tracklist tracks={playlist.tracks}></Tracklist>
-      <button className="Playlist-save-button">Save to Spotify!</button>
+      <button className="Playlist-save-button">Save to Spotify</button>
     </div>
   );
 };
