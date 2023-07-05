ca8e4fe0a681be5ea60caf1b991b6c5ddfa90147
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-04 22:22:55 -0300

    feat: adding some style to tracklist component

--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Track from '../Track/Track'
+import './Tracklist.scss'
 
 const Tracklist = ({tracks}) => {
   return (
     <div>
+      <h2 className='title'>Results</h2>
       {tracks.map((track) => {
         return <Track track={track} key={track.id}></Track>
       })}
