20aa62826b5a62a04cad782b7a81ed40a1c77829
João Vitor Vian <joaovitor_vian@hotmail.com>
2023-07-05 22:57:41 -0300

    Development (#11)
    * feat: clean up react app strutcture
    
    * feat: create empty track component
    
    * feat: create basic return statment and export default for my track component
    
    * feat: create basic tracklist component
    
    * feat: create basic playlist component
    
    * feat: create basic search results component
    
    * feat: create basic Search bar component
    
    * feat: add sass to project and change App background
    
    * feat: basic styling search bar component
    
    * fix: import search bar sass file
    
    * feat: implement components basic styles
    
    * feat: implement playlist component improvements styles
    
    * Feat/passing props (#8)
    
    * feat: passing hardcoded props throught components
    
    * feat: adding style to track component
    
    * fix: refactor track js class names
    
    * feat: adding some style to tracklist component
    
    * feat: adding playlist props and some styling refactoring (#9)
    
    * Feat/playlist states (#10)
    
    * style: change some values to buttons
    
    * refactor: add empty line at end of file
    
    * feat: implement provisory solution

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./SearchResults.scss";
+
+import TrackList from "../Tracklist/Tracklist";
+
+const SearchResults = (props) => {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList tracks={props.searchResults} onAdd={props.onAdd} />
+    </div>
+  );
+};
+
+export default SearchResults;
